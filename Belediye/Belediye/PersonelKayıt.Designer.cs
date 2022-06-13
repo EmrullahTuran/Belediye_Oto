0b7813d1cd000693728c56056eda7ca8a4e78b45
Emrullah Turan <emrhtrn49@gmail.com>
2022-06-13 23:40:20 +0300

    Personelkayıt sınıfı kodlamsı yapıldı

---
+++ Belediye/Belediye/PersonelKayıt.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Belediye
+{
+    partial class PersonelKayıt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PersonelKayıt));
+            this.btn_personel_kayit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtsoyad = new System.Windows.Forms.TextBox();
+            this.txtmeslek = new System.Windows.Forms.TextBox();
+            this.txtnumara = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnanasayfa = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_personel_kayit
+            // 
+            this.btn_personel_kayit.Location = new System.Drawing.Point(446, 290);
+            this.btn_personel_kayit.Name = "btn_personel_kayit";
+            this.btn_personel_kayit.Size = new System.Drawing.Size(75, 23);
+            this.btn_personel_kayit.TabIndex = 4;
+            this.btn_personel_kayit.Text = "Kaydol";
+            this.btn_personel_kayit.UseVisualStyleBackColor = true;
+            this.btn_personel_kayit.Click += new System.EventHandler(this.btn_personel_kayit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(324, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Adı :";
+            // 
+            // txtad
+            // 
+            this.txtad.Location = new System.Drawing.Point(380, 89);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(252, 23);
+            this.txtad.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(302, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Meslek : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(287, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Telefon no :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(307, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Soyadı :";
+            // 
+            // txtsoyad
+            // 
+            this.txtsoyad.Location = new System.Drawing.Point(380, 137);
+            this.txtsoyad.Name = "txtsoyad";
+            this.txtsoyad.Size = new System.Drawing.Size(252, 23);
+            this.txtsoyad.TabIndex = 1;
+            // 
+            // txtmeslek
+            // 
+            this.txtmeslek.Location = new System.Drawing.Point(380, 247);
+            this.txtmeslek.Name = "txtmeslek";
+            this.txtmeslek.Size = new System.Drawing.Size(252, 23);
+            this.txtmeslek.TabIndex = 3;
+            // 
+            // txtnumara
+            // 
+            this.txtnumara.Location = new System.Drawing.Point(380, 198);
+            this.txtnumara.Name = "txtnumara";
+            this.txtnumara.Size = new System.Drawing.Size(252, 23);
+            this.txtnumara.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(40, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(224, 214);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(192, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(256, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Personel Kayıt Ekranına Hoşgeldiniz";
+            // 
+            // btnanasayfa
+            // 
+            this.btnanasayfa.Location = new System.Drawing.Point(557, 290);
+            this.btnanasayfa.Name = "btnanasayfa";
+            this.btnanasayfa.Size = new System.Drawing.Size(75, 23);
+            this.btnanasayfa.TabIndex = 9;
+            this.btnanasayfa.Text = "Anasayfa";
+            this.btnanasayfa.UseVisualStyleBackColor = true;
+            this.btnanasayfa.Click += new System.EventHandler(this.btnanasayfa_Click);
+            // 
+            // PersonelKayıt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 390);
+            this.Controls.Add(this.btnanasayfa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtnumara);
+            this.Controls.Add(this.txtmeslek);
+            this.Controls.Add(this.txtsoyad);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_personel_kayit);
+            this.MaximumSize = new System.Drawing.Size(724, 429);
+            this.MinimumSize = new System.Drawing.Size(724, 429);
+            this.Name = "PersonelKayıt";
+            this.Text = "PersonelKayıt";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btn_personel_kayit;
+        private Label label1;
+        private TextBox txtad;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox txtsoyad;
+        private TextBox txtmeslek;
+        private TextBox txtnumara;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Button btnanasayfa;
+    }
+}
---
+++ Belediye/Belediye/PersonelKayıt.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Belediye
+{
+    public partial class PersonelKayıt : Form
+    {
+       
+        public PersonelKayıt()
+        {
+            InitializeComponent();
+        }
+        public SqlConnection con;
+        public SqlCommand cmd;
+        public SqlDataAdapter DA;
+        public DataSet DS;
+        public SqlDataReader DR;
+
+
+       
+        
+
+    private void btn_personel_kayit_Click(object sender, EventArgs e)
+        {
+
+            con = new SqlConnection(@"Data Source = IMPORTANTE\SQLEXPRESS; Initial Catalog = Belediye; Integrated Security = True");
+ 
+    
+
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+                SqlCommand veriekle = new SqlCommand("insert into PersonelKayit (Personel_adi,Personel_soyadi,Personel_numara,Personel_meslek) values (@adi,@soyadi,@numara,@meslek)", con);
+                veriekle.Parameters.AddWithValue("@adi", txtad.Text);
+                veriekle.Parameters.AddWithValue("@soyadi", txtsoyad.Text);
+                veriekle.Parameters.AddWithValue("@numara", txtnumara.Text);
+                veriekle.Parameters.AddWithValue("@meslek", txtmeslek.Text);
+                veriekle.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("Kayıt Başarılı");
+                txtad.Clear();
+                txtsoyad.Clear();
+                txtnumara.Clear();
+                txtmeslek.Clear();
+
+               
+            }
+
+            else
+            {
+                MessageBox.Show("Veri Ekleme Başarısız !");
+            }
+
+        }
+
+        private void btnanasayfa_Click(object sender, EventArgs e)
+        {
+            Anasayfa ana = new Anasayfa();
+            ana.Show();
+            this.Hide();
+        }
+    }
+}
