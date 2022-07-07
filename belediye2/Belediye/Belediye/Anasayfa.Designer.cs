5468f880fdf4a5c19956a89414da9eb28d68b9a4
Emrullah Turan <emrhtrn49@gmail.com>
2022-07-07 14:44:08 +0300

    anasayfa kodlamasının yapılması

---
+++ belediye2/Belediye/Belediye/Anasayfa.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Belediye
+{
+    partial class Anasayfa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPersonelEkle = new System.Windows.Forms.Button();
+            this.btnAracEkle = new System.Windows.Forms.Button();
+            this.datapersonel = new System.Windows.Forms.DataGridView();
+            this.dataArac = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.datapersonel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataArac)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPersonelEkle
+            // 
+            this.btnPersonelEkle.Location = new System.Drawing.Point(103, -3);
+            this.btnPersonelEkle.Name = "btnPersonelEkle";
+            this.btnPersonelEkle.Size = new System.Drawing.Size(109, 23);
+            this.btnPersonelEkle.TabIndex = 0;
+            this.btnPersonelEkle.Text = "Personel Ekle";
+            this.btnPersonelEkle.UseVisualStyleBackColor = true;
+            this.btnPersonelEkle.Click += new System.EventHandler(this.btnPersonelEkle_Click);
+            // 
+            // btnAracEkle
+            // 
+            this.btnAracEkle.Location = new System.Drawing.Point(230, -3);
+            this.btnAracEkle.Name = "btnAracEkle";
+            this.btnAracEkle.Size = new System.Drawing.Size(105, 23);
+            this.btnAracEkle.TabIndex = 3;
+            this.btnAracEkle.Text = "Araç Ekle";
+            this.btnAracEkle.UseVisualStyleBackColor = true;
+            this.btnAracEkle.Click += new System.EventHandler(this.btnAracEkle_Click);
+            // 
+            // datapersonel
+            // 
+            this.datapersonel.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.datapersonel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datapersonel.Location = new System.Drawing.Point(3, 45);
+            this.datapersonel.Name = "datapersonel";
+            this.datapersonel.RowTemplate.Height = 25;
+            this.datapersonel.Size = new System.Drawing.Size(601, 568);
+            this.datapersonel.TabIndex = 4;
+            this.datapersonel.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.datapersonel_CellContentClick);
+            // 
+            // dataArac
+            // 
+            this.dataArac.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataArac.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataArac.Location = new System.Drawing.Point(635, 45);
+            this.dataArac.Name = "dataArac";
+            this.dataArac.RowTemplate.Height = 25;
+            this.dataArac.Size = new System.Drawing.Size(494, 568);
+            this.dataArac.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(360, -3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Şikayet Öneri";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Anasayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1141, 660);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataArac);
+            this.Controls.Add(this.datapersonel);
+            this.Controls.Add(this.btnAracEkle);
+            this.Controls.Add(this.btnPersonelEkle);
+            this.Name = "Anasayfa";
+            this.Text = "Anasayfa";
+            this.Load += new System.EventHandler(this.Anasayfa_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.datapersonel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataArac)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnPersonelEkle;
+        private Button btnAracEkle;
+        private DataGridView datapersonel;
+        private DataGridView dataArac;
+        private Button button1;
+    }
+}
---
+++ belediye2/Belediye/Belediye/Anasayfa.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Belediye
+{
+    public partial class Anasayfa : Form
+    {
+        public Anasayfa()
+        {
+            InitializeComponent();
+        }
+        public SqlConnection con;
+        public SqlCommand cmd;
+        public SqlDataAdapter DA;
+        public DataSet DS;
+        public SqlDataReader DR;
+
+
+
+        private void btnPersonelEkle_Click(object sender, EventArgs e)
+        {
+            PersonelKayıt ps = new PersonelKayıt();
+            ps.Show();
+            this.Hide();
+
+        }
+
+        private void btnAracEkle_Click(object sender, EventArgs e)
+        {
+            AracBilgisi ar=new AracBilgisi();
+            ar.Show();
+            this.Hide();
+        }
+
+        private void Anasayfa_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection(@"Data Source = IMPORTANTE\SQLEXPRESS; Initial Catalog = Belediye; Integrated Security = True");
+            PersonelGetir();
+            AracGetir();
+
+        }
+        private void PersonelGetir()
+        {
+            con.Open();
+            string kayit = "SELECT * from PersonelKayit";
+            //musteriler tablosundaki tüm kayıtları çekecek olan sql sorgusu.
+            SqlCommand komut = new SqlCommand(kayit, con);
+            //Sorgumuzu ve baglantimizi parametre olarak alan bir SqlCommand nesnesi oluşturuyoruz.
+            SqlDataAdapter da = new SqlDataAdapter(komut);
+            //SqlDataAdapter sınıfı verilerin databaseden aktarılması işlemini gerçekleştirir.
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            //Bir DataTable oluşturarak DataAdapter ile getirilen verileri tablo içerisine dolduruyoruz.
+            datapersonel.DataSource = dt;
+            //Formumuzdaki DataGridViewin veri kaynağını oluşturduğumuz tablo olarak gösteriyoruz.
+            con.Close();
+        }
+        private void AracGetir()
+        {
+            con.Open();
+            string kayit = "SELECT * from AracBilgisi";
+            //musteriler tablosundaki tüm kayıtları çekecek olan sql sorgusu.
+            SqlCommand komut = new SqlCommand(kayit, con);
+            //Sorgumuzu ve baglantimizi parametre olarak alan bir SqlCommand nesnesi oluşturuyoruz.
+            SqlDataAdapter da = new SqlDataAdapter(komut);
+            //SqlDataAdapter sınıfı verilerin databaseden aktarılması işlemini gerçekleştirir.
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            //Bir DataTable oluşturarak DataAdapter ile getirilen verileri tablo içerisine dolduruyoruz.
+            dataArac.DataSource = dt;
+            //Formumuzdaki DataGridViewin veri kaynağını oluşturduğumuz tablo olarak gösteriyoruz.
+            con.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SikayetOneri si = new SikayetOneri();
+            si.Show();
+            this.Hide();
+        }
+
+        private void datapersonel_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
