f39bcc39bf092f605f4768638b60753c98f2455c
Emrullah Turan <emrhtrn49@gmail.com>
2022-07-09 12:25:49 +0300

    class1

---
+++ belediye2/Belediye/Belediye/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Belediye
+{
+    public class Class1
+    {
+        public SqlConnection con;
+        public SqlCommand cmd;
+        public SqlDataAdapter DA;
+        public DataSet DS;
+        public SqlDataReader DR;
+
+
+
+        public void baglanti()
+        {
+            con=new SqlConnection();
+            con.ConnectionString = @"Data Source=IMPORTANTE\SQLEXPRESS;Initial Catalog=Belediye;Integrated Security=True";
+            cmd.Connection = con;
+            con.Open();
+        }
+
+        
+    }
+}
